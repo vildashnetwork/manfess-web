4d00032ba086b63c9f4b0a9cdae021b0bd1b037a
Li Blissz <liblissz3@gmail.com>
2025-09-10 02:09:01 -0700

    server configuration4

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,9 @@ import './index.css'
 import App from './App.jsx'
 import {BrowserRouter } from "react-router-dom"
 createRoot(document.getElementById('root')).render(
-
+ 
     <BrowserRouter>
           <App />
     </BrowserRouter>
-
+  
 )
